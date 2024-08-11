eee4f1f758977ad42abd390c50b97ff5767dbb8f
Vaibhav Tiwari <122672330+Vaibhav-crux@users.noreply.github.com>
2024-08-11 22:42:08 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,17 +1,7 @@
 from fastapi import FastAPI
 
-from app.routers import (forbes, ambCrypto, blockWorks, coinDesk, coinGape, coinTelegraph, cryptoPotato, watcherGuru, beInCrypto, theDefiant)
+from app.routers import forbes
 
 app = FastAPI()
 
 app.include_router(forbes.router)
-app.include_router(ambCrypto.router)
-app.include_router(watcherGuru.router)
-app.include_router(blockWorks.router)
-app.include_router(coinDesk.router)
-app.include_router(coinGape.router)
-app.include_router(coinTelegraph.router)
-app.include_router(cryptoPotato.router)
-app.include_router(beInCrypto.router)
-app.include_router(theDefiant.router)
-
